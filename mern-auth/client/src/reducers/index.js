37e0b329b9ee7542067077cdc9f47339d483e0dd
Masaki Kudo <masaki.kudo@sjsu.edu>
2021-10-14 12:22:35 -0700

    Masaki's old codes deleted

--- mern-auth/client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import authReducers from "./authReducers";
-import errorReducers from "./errorReducers";
-export default combineReducers({
-  auth: authReducers,
-  errors: errorReducers
-});
--- mern-auth/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://user:sjsu@cluster0.ali5g.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
